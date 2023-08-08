2435a88554d5e038d1a7f65ed0eb32e9f19af888
2501jayanthi <2501jayanthi@gmail.com>
2023-08-08 04:08:42 +0000

    Password Validator

---
+++ src/components/PasswordValidator/index.js
@@ -0,0 +1,39 @@
+// Write your code here
+import {useState} from 'react'
+
+import {
+  PasswordBgContainer,
+  PasswordContainer,
+  Heading,
+  Text,
+  PasswordInput,
+  ErrorMessage,
+} from './styledComponents'
+
+const PasswordValidator = () => {
+  const [password, setPassword] = useState('')
+  const showErrorMessage = password.length < 8
+  const onChangePassword = event => {
+    setPassword(event.target.value)
+  }
+  return (
+    <PasswordBgContainer>
+      <PasswordContainer>
+        <Heading>Password Validator</Heading>
+        <Text>Check how strong and secure is your password</Text>
+        <PasswordInput
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+        />
+        {showErrorMessage && (
+          <ErrorMessage>
+            Your password must be at least 8 characters.
+          </ErrorMessage>
+        )}
+      </PasswordContainer>
+    </PasswordBgContainer>
+  )
+}
+
+export default PasswordValidator
